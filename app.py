ceebc232dc14fd5550e02eee95e339cd18a1eadf
Syed Mashood <88382605+mashinsp@users.noreply.github.com>
2023-12-20 14:46:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+
+# app.py (Flask part)
+
 from flask import Flask, render_template, request, jsonify
 import numpy as np
 from model import load_model
@@ -34,22 +37,14 @@ def predict():
                 city_encoded = label_encoder.transform([str(city)])[0]
                 area_type_encoded = label_encoder.transform([str(area_type)])[0]
             except ValueError as e:
-                
-                
-
                 new_labels = [label for label in [property_type, city, area_type] if label not in label_encoder.classes_]
                 label_encoder.classes_ = np.concatenate([label_encoder.classes_, new_labels])
                 property_type_encoded = label_encoder.transform([str(property_type)])[0]
                 city_encoded = label_encoder.transform([str(city)])[0]
                 area_type_encoded = label_encoder.transform([str(area_type)])[0]
 
-           
             input_array = np.array([property_type_encoded, city_encoded, baths, bedrooms, area_type_encoded, area_size]).reshape(1, -1)
-
-            
             prediction = loaded_model.predict(input_array)
-
-           
             predicted_price = f'Rs {prediction[0]:,.2f}'
 
             return jsonify({'prediction': predicted_price})
@@ -60,3 +55,4 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
